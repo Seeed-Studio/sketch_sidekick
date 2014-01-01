da3348bce5400e18195c68e07847056de4617191
reeedstudio <luweicong@qq.com>
2014-01-01 21:54:37 +0800

    add light sensor

---
+++ light_sensor/light_sensor.ino
@@ -0,0 +1,58 @@
+// demo of light sensor and led bar
+// light sensor connect to A0
+// led bar connect to D2~D11
+
+
+const int pinLightSensor = A0;
+
+// get val of analog(A0)
+int getAnalog()
+{
+	int sum = 0;
+	
+	for(int i=0; i<32; i++)
+	{
+		sum += analogRead(pinLightSensor);
+	}
+	
+	return sum>>5;
+}
+
+// set led bar, 0-10 level. 
+// 0 led for level-0 and 10 leds for level-10
+void setLight(int lev)
+{
+	for(int i=2; i<=11; i++)
+	{
+		digitalWrite(i, LOW);
+	}
+	
+	if(lev>0)
+	{
+		for(int i=0; i<lev; i++)
+		{
+			digitalWrite(i+2, HIGH);
+		}
+	}
+}
+
+
+void setup()
+{
+	for(int i=2; i<=11; i++)
+	{
+		pinMode(i, OUTPUT);
+		digitalWrite(i, LOW);
+	}
+}
+
+void loop()
+{
+	int val_light = getAnalog();					// get analog value
+	
+	int level = map(val_light, 0, 1023, 0, 10);		// make level
+	
+	setLight(level);								// set led bar
+	
+	
+}
