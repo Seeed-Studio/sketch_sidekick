705cab69fed0a9f7c9d98d49317a2c7dafecbcad
reeedstudio <luweicong@qq.com>
2014-01-01 22:01:57 +0800

    add sound sensor

--- sound_sensor/sound_sensor.ino
+++ sound_sensor/sound_sensor.ino
@@ -1,9 +1,9 @@
-// demo of light sensor and led bar
-// light sensor connect to A0
+// demo of sound sensor and led bar
+// sound sensor connect to A0
 // led bar connect to D2~D11
 
 
-const int pinLightSensor = A0;
+const int pinSoundSensor = A0;
 
 // get val of analog(A0)
 int getAnalog()
@@ -12,13 +12,13 @@ int getAnalog()
 	
 	for(int i=0; i<32; i++)
 	{
-		sum += analogRead(pinLightSensor);
+		sum += analogRead(pinSoundSensor);
 	}
 	
 	return sum>>5;
 }
 
-int getLightLevel()
+int getSoundLevel()
 {
 	int val_light = getAnalog();					// get analog value
 	int level = map(val_light, 0, 1023, 0, 10);		// make level
@@ -56,7 +56,7 @@ void setup()
 void loop()
 {
 
-	int level = getLightLevel();					// get light level
+	int level = getSoundLevel();					// get light level
 	
 	setLedBar(level);								// set led bar
 
